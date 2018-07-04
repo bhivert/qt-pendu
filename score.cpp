6d18e4ff05e606cfeb9003198d4b04e38b8359b8
Benoit HIVERT <Benoit.Hivert@hardis.fr>
2018-07-04 13:49:20 +0200

    score ui update

--- score.cpp
+++ score.cpp
@@ -1,11 +1,39 @@
 #include "score.h"
 #include "ui_score.h"
 
+#include <QFile>
+#include <QTextStream>
+#include <QStringList>
+#include <QTableWidgetItem>
+
+static QStringList _headers = {"Mot", "Coupa restant"};
+
 Score::Score(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::Score)
 {
     ui->setupUi(this);
+    ui->tableWidget->setColumnCount(2);
+    ui->tableWidget->setColumnWidth(0, 300);
+    ui->tableWidget->setHorizontalHeaderLabels(_headers);
+
+    QFile       file("score.csv");
+    QTextStream fstream(&file);
+    QString     line;
+    int         i = 0;
+
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+        return ;
+    while (!fstream.atEnd()) {
+        ui->tableWidget->setRowCount(i + 1);
+        line = fstream.readLine();
+        QStringList sp = line.split(';');
+        QTableWidgetItem *tmp = new QTableWidgetItem(sp[0]);
+        ui->tableWidget->setItem(i, 0, tmp);
+        tmp = new QTableWidgetItem(sp[1]);
+        ui->tableWidget->setItem(i, 1, tmp);
+        ++i;
+    }
 }
 
 Score::~Score()
